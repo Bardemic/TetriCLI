2564fe560fee9a11246578f2b712b844791f2a0b
Bardemic <134565367+Bardemic@users.noreply.github.com>
2024-12-06 05:24:40 -0600

    Left Right Collision Logic, Add Basic Movement

--- main.c
+++ main.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-#define ROWS 8
-#define COLS 7
+#define ROWS 10
+#define COLS 8
 #define BLOCK 4
 #define EMPTY 0
 struct Block {
@@ -25,13 +25,17 @@ void dropBlock(struct Block *curBlock, int (*array)[COLS]);
 void tetrisCheck(int (*array)[COLS]);
 void placeBlock(int (*array)[COLS], struct Block curBlock);
 int checkCollisionDrop(struct Block curBlock, int (*array)[COLS]);
-void printGame(int (*array)[COLS]);
+void printGame(int (*array)[COLS], int score);
 int playGame();
-
+int checkCollisionLR(struct Block curBlock, int (*array)[COLS], int direction /*1 for right, -1 for left*/);
+void shiftBlock(struct Block *curBlock, int (*array)[COLS], int direction /*1 for right, -1 for left*/);
 
 
 int main(int argc, char *argv[]){
     int xButton, circleButton, triangleButton, squareButton;
+    int score = 0;
+    double timePassed = 0;
+    double timeToPass = 0.5;
     struct Block b1 = blocks[rand() % 5];
 
     if(playGame()){
@@ -44,10 +48,16 @@ int main(int argc, char *argv[]){
         }
         while(1){
             
-            placeBlock(game_array, b1);
-            printGame(game_array);
-            dropBlock(&b1, game_array);
-            sleep(1);
+            timePassed += 0.000000005;
+            if(timePassed >= timeToPass){
+                timeToPass += 1;
+                int direction = 0;
+                scanf("%d", &direction);
+                shiftBlock(&b1, game_array, direction);
+                placeBlock(game_array, b1);
+                printGame(game_array, score); //all this does is print the array, dw about logiccc
+                dropBlock(&b1, game_array);
+            }
             
         }
 
@@ -58,6 +68,20 @@ int main(int argc, char *argv[]){
     return 0;
 }
 
+void shiftBlock(struct Block *curBlock, int (*array)[COLS], int direction /*1 for right, -1 for left*/){
+    int count = 0;
+    for(int i = 0; i < 4; i++){
+        if(checkCollisionLR(*curBlock, array, direction)){
+            count++;
+        }
+    }
+    if(count == 0){
+        for(int i = 0; i < 4; i++){
+            array[curBlock->shape[i][0] + curBlock->yPosition][curBlock->shape[i][1] + curBlock->xPosition] = EMPTY;
+            curBlock->shape[i][1] += direction;
+        }
+    }
+}
 
 void dropBlock(struct Block *curBlock, int (*array)[COLS]){
     int count = 0;
@@ -101,6 +125,27 @@ void tetrisCheck(int (*array)[COLS]){
     }
 }
 
+int checkCollisionLR(struct Block curBlock, int (*array)[COLS], int direction /*1 for right, -1 for left*/){
+    for(int i = 0; i < 4; i++){
+        int curCol = curBlock.shape[i][1] + curBlock.xPosition;
+        if(curCol + direction < 0 || curCol + direction >= COLS){
+            return 1;
+        }
+        if(array[curBlock.shape[i][0] + curBlock.yPosition][curCol + direction] == BLOCK){
+            int testCount = 0; //is 0 when block in the column of the desired direction is not in the current full block, is 1 when it is
+            for(int j = 0; j < 4; j++){
+                if(curBlock.shape[j][0] + curBlock.yPosition == curBlock.shape[i][0] + curBlock.yPosition && curBlock.shape[j][1] + curBlock.xPosition == curCol + direction){
+                    testCount++;
+                }
+            }
+            if(testCount != 1){
+                return 1;
+            }
+        }
+    }
+    return 0;
+}
+
 int checkCollisionDrop(struct Block curBlock, int (*array)[COLS]){
     for(int i = 0; i < 4; i++){
         int curRow = curBlock.shape[i][0] + curBlock.yPosition;
@@ -130,7 +175,7 @@ void placeBlock(int (*array)[COLS], struct Block curBlock){
 }
 
 
-void printGame(int (*array)[COLS]){
+void printGame(int (*array)[COLS], int score){
     system("clear");
     for(int i = 0; i < ROWS; i++){
         for(int j = 0; j < COLS; j++){
@@ -138,6 +183,11 @@ void printGame(int (*array)[COLS]){
         }
         printf("\n");
     }
+    for(int i = 0; i < COLS; i++){
+        printf("--");
+    }
+    printf("\n");
+    printf("Score: %d\n", score);
 }
 
 
